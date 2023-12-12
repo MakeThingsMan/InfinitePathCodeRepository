a05d2f0c52af83ec5daeb95c1ad9f3509c304cb6
MakeThingsMan <145855417+MakeThingsMan@users.noreply.github.com>
2023-12-12 14:07:42 -0500

    Item Id Function Change
    Changed the ID function into a proper, yet simple, hashing function with a decryption function as well.

--- ServerSideControllers/DataControl/Classes/Items/Item.lua
+++ ServerSideControllers/DataControl/Classes/Items/Item.lua
@@ -42,21 +42,38 @@ function Item.new(name,Type,appearance,weight,maxStacks,stacks,rank,value)
 	newItem.Stacks 		= stacks
 	newItem.Rank		= rank
 	newItem.InvLocation	= {nil,nil}
-	newItem.Id			= IdFunction(newItem)
+	newItem.Id		= IdFunction(newItem)
 	setmetatable(newItem,Item)
 	--print(newItem)
 	return newItem
 end
 
-function IdFunction(Item)
+function IdFunction(Item) 
+	-- turn this into a hashing function rather than whatever this is. 
+	-- Make your own hash function even if it's simple because you need to be able to encrypt one for every
 	if Item.Name == " " then return 0 end
 	
 	local temp = ""
+	local baseTable= {a,b,c,d,e,f,g,h,I,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9,0}
+	local keyTable = {q,a,z,x,s,w,e,d,c,v,f,r,t,g,b,n,h,y,u,j,m,I,k,o,l,p,9,2,3,5,6,4,1,0,8,7}
+	-- to increase the complexity you ould then shift the letters all over by some amount and slap that number at the 
+	-- end of the hashed string and have the decrypter do the shifting in reverse. 
+	local salt = "C%1(k3-" 
 	for i =1, #Item.Name,1 do
-		local x = math.random(33,126)
-		temp = temp..tostring(x)
+		temp += keyTable[IndexOf(Item.Name[i])]
 	end 
+	local x = math.random(1,#Item.Name)
+	
+	temp += String.sub(1,x) .. salt .. String.sub(x,#Item.Name)
 	return temp
+end
+
+function DecryptId(Input)
+	local baseTable		= {a,b,c,d,e,f,g,h,I,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9,0}
+	local keyTable 		= {q,a,z,x,s,w,e,d,c,v,f,r,t,g,b,n,h,y,u,j,m,I,k,o,l,p,9,2,3,5,6,4,1,0,8,7}
+	local salt 		= "C%1(k3-"
+	local saltLocation 	= String.Find(Input,salt)
+	return String.sub(1,saltLocation) .. String.sub(saltLocation+7,Input.Length)
 end
 
 function Item.NewEmpty()
