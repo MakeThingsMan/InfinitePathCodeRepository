896f47e6dd0c68502a09ab2711a389ff92d66c90
MakeThingsMan <145855417+MakeThingsMan@users.noreply.github.com>
2023-12-12 23:40:34 -0500

    Update Item.lua

--- ServerSideControllers/DataControl/Classes/Items/Item.lua
+++ ServerSideControllers/DataControl/Classes/Items/Item.lua
@@ -52,35 +52,38 @@ function IdFunction(Item)
 	-- turn this into a hashing function rather than whatever this is. 
 	-- Make your own hash function even if it's simple because you need to be able to encrypt one for every
 	if Item.Name == " " then return 0 end
-	
+
 	local temp = ""
-	local baseTable= {a,b,c,d,e,f,g,h,I,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9,0}
-	local keyTable = {q,a,z,x,s,w,e,d,c,v,f,r,t,g,b,n,h,y,u,j,m,I,k,o,l,p,9,2,3,5,6,4,1,0,8,7}
+	local baseTable= {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"}
+	local keyTable = {"q","a","z","x","s","w","e","d","c","v","f","r","t","g","b","n","h","y","u","j","m","i","k","o","l","p","9","2","3","5","6","4","1","0","8","7"}
 	-- to increase the complexity you ould then shift the letters all over by some amount and slap that number at the 
 	-- end of the hashed string and have the decrypter do the shifting in reverse. 
 	local salt = "C%1(k3-" 
 	for i =1, #Item.Name,1 do
-		temp += keyTable[IndexOf(Item.Name[i],baseTable)]
+		print(i,#Item.Name,keyTable[table.find(baseTable,string.sub(string.lower(Item.Name),i,i))],temp)
+		print(string.sub(string.lower(Item.Name),i,i),Item.Name)
+		temp = temp.. keyTable[table.find(baseTable,string.sub(string.lower(Item.Name),i,i))]
 	end 
 	local x = math.random(1,#Item.Name)
-	
-	temp += String.sub(1,x) .. salt .. String.sub(x,#Item.Name)
+
+	temp = string.sub(temp,1,x) .. salt .. string.sub(temp,x,#Item.Name)
 	return temp
 end
 
 function DecryptId(Input)
-	local baseTable		= {a,b,c,d,e,f,g,h,I,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9,0}
-	local keyTable 		= {q,a,z,x,s,w,e,d,c,v,f,r,t,g,b,n,h,y,u,j,m,I,k,o,l,p,9,2,3,5,6,4,1,0,8,7}
+	local baseTable= {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"}
+	local keyTable = {"q","a","z","x","s","w","e","d","c","v","f","r","t","g","b","n","h","y","u","j","m","i","k","o","l","p","9","2","3","5","6","4","1","0","8","7"}
 	local salt 		= "C%1(k3-"
-	local saltLocation 	= String.Find(Input,salt)
-	Input 			= String.sub(1,saltLocation) .. String.sub(saltLocation+7,Input.Length)
+	local saltLocation 	= string.find(Input,salt)
+	Input 			= string.sub(Input,1,saltLocation) .. string.sub(Input,saltLocation+7,Input.Length)
 	local temp		= ""
 	for i=1, Input.Length do 
-		temp += baseTable[IndexOf(Item.Name[i],keyTable)]
+		temp = temp.. baseTable[table.find(keyTable,string.sub(string.lower(Input),i,i))]
 	end
-	
-	return temp`1	
+
+	return temp
 end
+
 
 function Item.NewEmpty()
 	local newitem = Item.new(" ","Empty",nil,0,0,0,0)
